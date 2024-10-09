492b3467b7314f584aa7e118bfa7e6071cade7ef
Pyriac <cyriacpineau@gmail.com>
2024-10-09 11:54:07 +0200

    FEATURE : data calcul on front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import "./assets/styles/root.css";
+import "./assets/styles/home.css";
 
 function App() {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,19 @@ import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
   const notesData = useLoaderData();
+  const sport = notesData.reduce((acc, note) => acc + note.sport, 0);
+  const sleep =
+    notesData.reduce((acc, note) => acc + note.sleep, 0) / notesData.length;
+  const mental =
+    notesData.reduce((acc, note) => acc + note.mental, 0) / notesData.length;
   console.info(notesData);
-  return <h1>Hello Wolrd</h1>;
+  return (
+    <>
+      <h2>Bonjour User 👋</h2>
+      <h3>Comment ce sont passés tes derniers jours :</h3>
+      <h4>{sport}</h4>
+      <h4>{sleep}</h4>
+      <h4>{mental}</h4>
+    </>
+  );
 }
